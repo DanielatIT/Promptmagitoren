60fb0051220f30f1cf18c37a5d8e6d2de26eb391
Daniel Wolfing <daniel.wolfing.dm@gmail.com>
2025-08-01 11:38:57 +0000

    När jag lägger upp denna på github och sen pushar den till min vercel ko

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
+import '@/ai/genkit';
 
 export const metadata: Metadata = {
   title: 'PromptSmith',
