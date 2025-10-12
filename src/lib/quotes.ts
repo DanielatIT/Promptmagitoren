3c48e8ac0f3131097e8ce2a0eb3cd439ffeff580
Daniel Wolfing <daniel.wolfing.dm@gmail.com>
2025-10-12 08:41:45 +0000

    Utöver det som redan finns lägg in dessa: “Det var inte jag, Det va stef

--- src/lib/quotes.ts
+++ src/lib/quotes.ts
@@ -119,5 +119,121 @@ export const quotes = [
     {
         quote: "Är detta förlust?",
         source: "Ctrl+Alt+Del"
+    },
+    {
+        quote: "Det var inte jag, Det va stefan",
+        source: "Victor Lindström"
+    },
+    {
+        quote: "Ja jag bodde i Falunnn",
+        source: "Bullbin"
+    },
+    {
+        quote: "Jag kan umbraco SUPER mega bra! Är proffs.",
+        source: "Daniel Wölfing"
+    },
+    {
+        quote: "Vet inte vad dom ville denna gången. Men bad mig åka ner till Halmstad. För nått extra insatt stormöte.",
+        source: "EJ"
+    },
+    {
+        quote: "Va? ska du också på stormöte?",
+        source: "EN"
+    },
+    {
+        quote: "That's what she said.",
+        source: "Michael Scott"
+    },
+    {
+        quote: "I am Beyoncé, always.",
+        source: "Michael Scott"
+    },
+    {
+        quote: "I'm not superstitious, but I am a little stitious.",
+        source: "Michael Scott"
+    },
+    {
+        quote: "Bears. Beets. Battlestar Galactica.",
+        source: "Jim Halpert"
+    },
+    {
+        quote: "I talk a lot, so I've learned to tune myself out.",
+        source: "Kelly Kapoor"
+    },
+    {
+        quote: "I wish there was a way to know you’re in the good old days before you’ve actually left them.",
+        source: "Andy Bernard"
+    },
+    {
+        quote: "I declare bankruptcy!",
+        source: "Michael Scott"
+    },
+    {
+        quote: "Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.",
+        source: "Michael Scott"
+    },
+    {
+        quote: "I understand nothing.",
+        source: "Michael Scott"
+    },
+    {
+        quote: "I am running away from my responsibilities. And it feels good.",
+        source: "Michael Scott"
+    },
+    {
+        quote: "I love inside jokes. I’d love to be a part of one someday.",
+        source: "Michael Scott"
+    },
+    {
+        quote: "Identity theft is not a joke, Jim!",
+        source: "Dwight Schrute"
+    },
+    {
+        quote: "Whenever I'm about to do something, I think, 'Would an idiot do that?' and if they would, I do not do that thing.",
+        source: "Dwight Schrute"
+    },
+    {
+        quote: "I am fast. To give you a reference point, I am somewhere between a snake and a mongoose… and a panther.",
+        source: "Dwight Schrute"
+    },
+    {
+        quote: "I’m not usually the 'butt' of the joke. I’m usually the 'face' of the joke.",
+        source: "Michael Scott"
+    },
+    {
+        quote: "I feel God in this Chili’s tonight.",
+        source: "Pam Beesly"
+    },
+    {
+        quote: "I'm dead inside.",
+        source: "Stanley Hudson"
+    },
+    {
+        quote: "How the turntables...",
+        source: "Michael Scott"
+    },
+    {
+        quote: "Through concentration, I can raise and lower my cholesterol at will.",
+        source: "Dwight Schrute"
+    },
+    {
+        quote: "I say dance, they say ‘How high?’",
+        source: "Michael Scott"
+    },
+    {
+        quote: "STÄNG FÖNSTRET!!!",
+        source: "Adam"
+    },
+    {
+        quote: "Jag ligger först på 3d-printing",
+        source: "Lenny Benny"
+    },
+    {
+        quote: "Fernet",
+        source: "Eric Manni"
+    },
+    {
+        quote: "Bougatti diva",
+        source: "Viktor Eriksson"
     }
 ];
